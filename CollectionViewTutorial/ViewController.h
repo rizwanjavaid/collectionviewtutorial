badd61ce0a34cbf65a31625bf1fee18fddc48d38
Rizwan Javaid <rizwan@rizwanjavaid.com>
2013-07-23 18:34:05 -0700

    Initial Commit

---
+++ CollectionViewTutorial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CollectionViewTutorial
+//
+//  Created by Rizwan Javaid-Old on 7/23/13.
+//  Copyright (c) 2013 Rizwan Javaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CollectionViewTutorialTests/CollectionViewTutorialTests.m
@@ -0,0 +1,36 @@
+//
+//  CollectionViewTutorialTests.m
+//  CollectionViewTutorialTests
+//
+//  Created by Rizwan Javaid-Old on 7/23/13.
+//  Copyright (c) 2013 Rizwan Javaid. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CollectionViewTutorialTests : XCTestCase
+
+@end
+
+@implementation CollectionViewTutorialTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
